c08c3b6d526ea15482e0c20ee71ef852fed5e89d
Sean Connelley <connelley@stamen.com>
2015-06-01 13:50:16 -0400

    Diary writer list rows respond to trail selection

--- app/components/ListView/Item.jsx
+++ app/components/ListView/Item.jsx
@@ -11,9 +11,11 @@ var Item = React.createClass({
   },
 
   render: function() {
+    var trailClass = this.props.item.trail.toLowerCase().replace(' ', '-');
 
     return (
-      <li className="list-group-item pointer" onClick={this.toggle}>
+      <li className={"list-group-item pointer " + trailClass} onClick={this.toggle}>
+        <div className="list-group-disabler"></div>
         <p><span className="item-name">{this.props.item.name}</span> - <span className="item-trail">{this.props.item.trail}</span></p>
         <p>Diary begins {dateFormat(this.props.item.begins)} ({this.props.item.values.length} entries)</p>
       </li>
